4ba9504c71fc8b169821c076d2c749e3a2177a8e
jseo123 <jose123135@gmail.com>
2022-01-16 10:38:58 +0100

    Change the addToDatabase logic

--- src/admin/control/addRestaurant.php
+++ src/admin/control/addRestaurant.php
@@ -1,43 +1,11 @@
 <?php
 if (isset($_POST["cvc"])) {
-  $data = $_POST;
-  $firstName = $data["ownerName"];
-  $lastName = $data["lastName"];
-  $restaurantName = $data["restaurantName"];
-  $adress = $data["adress"];
-  $phone = $data["phone"];
-  $cardNumber = $data["cardNumber"];
-  $cvc = $data["cvc"];
-  $expirationDate = $data["expirationDate"];
-  $email = $data["email"];
-  $user = $data["user"];
-  $pass = password_hash($data["pass"], PASSWORD_DEFAULT);
-  $notes = $data["notes"];
+require_once "./databaseManager.php";
+  addRestaurant($_POST);
   unset($_POST);
-  //defines server variables for sql.
-  $servername = "localhost";
-  $username = "Jose";
-  $password = ".12.,12jose";
-  $dbname = "emenu";
-
-  // Create connection
-  $conn = mysqli_connect($servername, $username, $password, $dbname);
-
-  // Check connection
-  if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-  }
-
-  $sql = "INSERT INTO restaurants (first_name, last_name, restaurant_name, adress, phone, card_number, cvc, expiration_date, email, user, password, notes, creation_date)
-VALUES ('$firstName', '$lastName', '$restaurantName', '$adress', '$phone', '$cardNumber', '$cvc', '$expirationDate', '$email', '$user', '$pass', '$notes', now())";
-
-if ($conn->query($sql) === TRUE) {
-header("Location: ../newRestaurant.php?restaurantAdded");
-} else {
-  header("Location: ../newRestaurant.php?error");
 }
 
-}
+
 
 
 
--- src/admin/control/databaseManager.php
+++ src/admin/control/databaseManager.php
@@ -6,3 +6,44 @@ function confirmationMessage(){
 function errorMessage(){
     echo "<p class ='alert alert-danger d-flex justify-content-center'>!Error al rellenar los datos!</p>";
 }
+
+function addRestaurant($_POST){
+    $data = $_POST;
+  $firstName = $data["ownerName"];
+  $lastName = $data["lastName"];
+  $restaurantName = $data["restaurantName"];
+  $adress = $data["adress"];
+  $phone = $data["phone"];
+  $cardNumber = $data["cardNumber"];
+  $cvc = $data["cvc"];
+  $expirationDate = $data["expirationDate"];
+  $email = $data["email"];
+  $user = $data["user"];
+  $pass = password_hash($data["pass"], PASSWORD_DEFAULT);
+  $notes = $data["notes"];
+  unset($_POST);
+  //defines server variables for sql.
+  $servername = "localhost";
+  $username = "Jose";
+  $password = ".12.,12jose";
+  $dbname = "emenu";
+  $duplicateError = 1062;
+
+  // Create connection
+  $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+  // Check connection
+  if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+  }
+
+  $sql = "INSERT INTO restaurants (first_name, last_name, restaurant_name, adress, phone, card_number, cvc, expiration_date, email, user, password, notes, creation_date)
+VALUES ('$firstName', '$lastName', '$restaurantName', '$adress', '$phone', '$cardNumber', '$cvc', '$expirationDate', '$email', '$user', '$pass', '$notes', now())";
+
+if ($conn->query($sql) === TRUE) {
+header("Location: ../newRestaurant.php?restaurantAdded");
+} else {
+  header("Location: ../newRestaurant.php?error");
+}
+
+}
